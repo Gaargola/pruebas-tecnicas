346a45e8a1c98f4eae21fa5aba746886fe9b1ff9
Gaargola <tl3pr0yt@gmail.com>
2023-07-20 12:01:50 -0300

    Sincronizacion e inicio de proyecto

--- pruebas/01-reading-list/gaargola/src/app/page.tsx
+++ pruebas/01-reading-list/gaargola/src/app/page.tsx
@@ -1,9 +1,12 @@
-export default function Home() {
-  return (
-  <main>
-    <div>
-      
-    </div>
-  </main>
-  );
+async function getData() {
+  const res = await fetch("./data/books.json");
+  if (!res.ok) {
+    throw new Error(`Failed to fetch data, received ${res.status}`);
+  }
+  return res.json();
+}
+
+export default async function Page() {
+  const data = await getData();
+  return <main>{data}</main>;
 }
